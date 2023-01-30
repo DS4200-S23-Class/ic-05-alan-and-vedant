218f5d4e22e8464cdcb889030c4d680268cbab98
vbhagat02 <vedant.bhagat0204@gmail.com>
2023-01-30 16:41:31 -0500

    final changes

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let clicks = 0
 
 function buttonClicked() {
     clicks += 1
+    console.log(clicks)
     let newText = "Number of times button clicked: " + clicks;
     let buttonDiv = document.getElementById("button-div");
     buttonDiv.innerHTML = newText;
-
 }
